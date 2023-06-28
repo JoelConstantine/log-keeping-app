77c824ba4da37449f0b382246df756cc2d01fe8d
Joel <joel@joelconstantine.com>
2023-06-28 16:06:31 -0500

    Added Vuetify, began work on logbook layout page

---
+++ src/views/JournalView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const welcome_message = "Hello World"
+import LogBookView from './LogBookView.vue'
+</script>
+
+<template>
+    <v-row>
+        <v-col cols="4">
+            <LogBookView title="Garden"></LogBookView>
+        </v-col>
+    </v-row>
+</template>
---
+++ src/views/LogBookView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps({
+    title: {
+        type: String,
+        default: ""
+    }
+})
+</script>
+
+<template>
+    <v-card>
+        <v-card-title>{{ title }}</v-card-title>
+        <v-card-text>
+        </v-card-text>
+    </v-card>
+</template>
